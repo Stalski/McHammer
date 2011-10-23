5d04f939d61d741155e4464f01cbe7e84596426b
Stalski <jochen@menhir.be>
2011-10-24 00:42:51 +0200

    Added mail newsletter page.

--- plugins/layouts/onecol_mailtemplate/panels-onecol-mailtemplate.tpl.php
+++ plugins/layouts/onecol_mailtemplate/panels-onecol-mailtemplate.tpl.php
@@ -14,6 +14,6 @@
 ?>
 <div class="panel-display panel-1col clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
   <div class="panel-panel panel-col">
-    <div><?php print $content['middle']; ?></div>
+    <?php print $content['middle']; ?>
   </div>
 </div>
